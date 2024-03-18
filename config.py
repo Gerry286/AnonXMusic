3ed96d3d36f5e5e85eedeebe03e5ca84905a51bf
Kouji <157406595+koji280@users.noreply.github.com>
2024-03-18 23:38:42 +0530

    Update config.py
    Signed-off-by: Kouji <157406595+koji280@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/the_Reaperss")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Entertainment_AG")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/the_Reaperss")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
@@ -81,17 +81,17 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://graph.org/file/4607bda35c9f033e0648c.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/4607bda35c9f033e0648c.jpg"
+    "PING_IMG_URL", "https://graph.org/file/306b51871133e9f382605.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/fb5d4b5991e06cb5c6abc.jpg"
 STATS_IMG_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
 TELEGRAM_VIDEO_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
 STREAM_IMG_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/18e55ddf319a3f18ac6cb.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/18e55ddf319a3f18ac6cb.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/306b51871133e9f382605.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/306b51871133e9f382605.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/4607bda35c9f033e0648c.jpg"
 
 
